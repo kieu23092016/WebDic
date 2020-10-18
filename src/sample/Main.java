3425ab00c7962fd30f4d2e18067d13fca2e52b1f
Duyen <duyeenlee2072001@gmail.com>
2020-10-19 01:09:54 +0700

    Thêm sửa xóa cơ bản

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -16,31 +16,44 @@ import javafx.stage.Stage;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Optional;
+import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main extends Application {
 
+    private WebEngine webEngine;
     @FXML
     private TextField word;
     @FXML
     private ListView<String> listWord;
-    private ListView<String> lisCheck;
     @FXML
     private WebView definitionWord;
-    private WebEngine webEngine;
     @FXML
-    private Button search;
+    private TextField addedWord;
+    @FXML
+    private TextArea meaningText;
+    @FXML
+    private TextField editWord;
+    @FXML
+    private TextArea editMeaning;
+
     static Map<String, String> myTranslate = new TreeMap<String, String>();
     static final String DATA_FILE_PATH = "D:\\clone\\WebDic\\src\\sample\\E_V.txt";
     static final String SPLITTING_CHARACTERS = "<html>";
     private ObservableList<String> observableList = FXCollections.observableList(new ArrayList<String>());
+    private ObservableList<String> beginListView = FXCollections.observableList(new ArrayList<String>());
 
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * lấy dữ liệu từ điển từ file.
+     */
     public void insertFromFile() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(DATA_FILE_PATH));
         String currentLine;
@@ -53,62 +66,156 @@ public class Main extends Application {
         reader.close();
     }
 
+    /**
+     * tìm kiếm chính xác nghĩa của 1 từ tiếng Anh.
+     */
     public String dictionaryLookup(String key) {
         if (myTranslate.containsKey(key))
             return myTranslate.get(key);
         return "Not Found";
     }
 
-    public void submit(ActionEvent event){
-        if (!word.getText().isEmpty()){
+    /**
+     * tìm kiếm gần đúng một từ.
+     */
+    public void submit(ActionEvent event) {
+        if (!word.getText().isEmpty()) {
             String textSearch = word.getText();
-            Pattern pattern = Pattern.compile("\\b"+textSearch, Pattern.CASE_INSENSITIVE);
-            if(!observableList.isEmpty()) {
+            Pattern pattern = Pattern.compile("\\b" + textSearch, Pattern.CASE_INSENSITIVE);
+            if (!observableList.isEmpty()) {
                 observableList.clear();
             }
-            for(String key : myTranslate.keySet()) {
+            for (String key : myTranslate.keySet()) {
                 Matcher matcher = pattern.matcher(key);
-                if(matcher.find()){
+                if (matcher.find()) {
                     System.out.println(key);
                     observableList.add(key);
-                    listWord.setItems(observableList);
                 }
+            }
+            listWord.setItems(observableList);
+        }
+    }
+
+    /**
+     * xóa một từ tiếng Anh khỏi từ điển.
+     */
+    public void delete(ActionEvent event) {
+        String key = listWord.getSelectionModel().getSelectedItem();
+        if (!key.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Delete word.");
+            alert.setHeaderText("Are you sure you want to remove this word?");
+            alert.setContentText(key);
+
+            Optional<ButtonType> optionalButtonType = alert.showAndWait();
+            Alert child_alert = new Alert(Alert.AlertType.INFORMATION);
+            if (optionalButtonType.get() == ButtonType.OK) {
+                myTranslate.remove(key);
+
+                reLoadListView(key);
+
+                child_alert.setHeaderText("Delete Successfully!");
+                child_alert.showAndWait();
             }
         }
     }
 
-    public void delete(ActionEvent event) {
-            String key = listWord.getSelectionModel().getSelectedItem();
-            if (!key.isEmpty()){
-                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                alert.setTitle("Delete word.");
-                alert.setHeaderText("Are you sure you want to remove this word?");
-                alert.setContentText(key);
+    /**
+     * mở hành động sửa dữ liệu của 1 từ tiếng Anh.
+     */
+    public void edit(ActionEvent event) throws IOException {
 
-                Optional<ButtonType> optionalButtonType = alert.showAndWait();
-                Alert child_alert = new Alert(Alert.AlertType.INFORMATION);
-                if (optionalButtonType.get() == ButtonType.OK) {
-                    myTranslate.remove(key);
-                    observableList.remove(key);
-                    webEngine = definitionWord.getEngine();
-                    webEngine.loadContent("");
+        String key = listWord.getSelectionModel().getSelectedItem();
+        if (!key.isEmpty()) {
+            /**
+             * tạo một cửa sổ mới để sửa từ. ***********************
+             */
+            Stage secondStage = new Stage();
+            Parent root1 = FXMLLoader.load(getClass().getResource("editor.fxml"));
+            Scene secondScene = new Scene(root1);
+            secondStage.setScene(secondScene);
+            secondStage.setTitle("Editor");
 
-                    child_alert.setHeaderText("Delete Successfully!");
-                    child_alert.showAndWait();
-                }
-            }
+            secondStage.show();
+            editWord = (TextField) secondScene.lookup("#editWord");
+            editMeaning = (TextArea) secondScene.lookup("#editMeaning");
+            /********************************************************/
+            editWord.setText(key);
+            editMeaning.setText(myTranslate.get(key));
+
+            myTranslate.remove(key);
+            reLoadListView(key);
+        }
+    }
+
+    /**
+     * lưu lại hành động sửa từ.
+     */
+    public void saveEdit(ActionEvent event) {
+        if (!editWord.getText().isEmpty() && !editMeaning.getText().isEmpty()) {
+            String k = editWord.getText();
+            String v = editMeaning.getText();
+            myTranslate.put(k, v);
+            beginListView.add(k);
+
+            Alert editingInfo = new Alert(Alert.AlertType.INFORMATION);
+            editingInfo.setContentText("Edit successfully!");
+            editingInfo.showAndWait();
+        }
+    }
+
+    /**
+     * mở hành động thêm một từ tiếng Anh vào từ điển.
+     */
+    public void addWord(ActionEvent event) throws IOException {
+        /**
+         * tạo 1 cửa sổ mới để thêm từ tiếng Anh.***************
+         */
+        Stage secondStage = new Stage();
+        Parent root1 = FXMLLoader.load(getClass().getResource("child.fxml"));
+        Scene secondScene = new Scene(root1);
+        secondStage.setScene(secondScene);
+        secondStage.setTitle("Add a word");
+        secondStage.show();
+        /**
+         * *****************************************************
+         */
+    }
+
+    /**
+     * lưu hành động sửa.
+     */
+    public void saveAdd(ActionEvent event) {
+        if (!addedWord.getText().isEmpty() && !meaningText.getText().isEmpty()) {
+            String key = addedWord.getText();
+            String value = meaningText.getText();
+            myTranslate.put(key, value);
+            observableList.add(key);
+
+            Alert addingInfo = new Alert(Alert.AlertType.INFORMATION);
+            addingInfo.setContentText("Add successfully!");
+            addingInfo.showAndWait();
+        }
+    }
+
+    /**
+     * hiển thị toàn bộ danh sách từ điển.
+     */
+    public void showAllWords() {
+        beginListView.setAll(myTranslate.keySet());
+        listWord.setItems(beginListView);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Dictionary");
         Scene scene = new Scene(root, 1077, 552);
         primaryStage.setScene(scene);
         primaryStage.show();
-        initComponents(scene);
         insertFromFile();
 
+        initComponents(scene);
         loadWordOnList();
 
     }
@@ -117,17 +224,30 @@ public class Main extends Application {
         definitionWord = (WebView) scene.lookup("#definitionWord");
         listWord = (ListView<String>) scene.lookup("#listWord");
     }
-    
+
+    /**
+     * lấy nghĩa của từ khi click chuột vào từ.
+     */
     public void loadWordOnList() {
-            listWord.setOnMouseClicked(event1 -> {
-                String key = listWord.getSelectionModel().getSelectedItem();
-                if (!key.isEmpty()){
-                    webEngine = definitionWord.getEngine();
-                    String content = dictionaryLookup(key);
-                    webEngine.loadContent(content);
-                }
+        listWord.setOnMouseClicked(event1 -> {
+            String key = listWord.getSelectionModel().getSelectedItem();
+            if (!key.isEmpty()) {
+                webEngine = definitionWord.getEngine();
+                String content = dictionaryLookup(key);
+                webEngine.loadContent(content);
+            }
 
-            });
+        });
 
+    }
+
+    /**
+     * cập nhật lại list view khi xóa một từ.
+     */
+    public void reLoadListView(String key) {
+        observableList.remove(key);
+        beginListView.remove(key);
+        webEngine = definitionWord.getEngine();
+        webEngine.loadContent("");
     }
 }
